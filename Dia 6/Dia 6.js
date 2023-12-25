8c21671b74ab1961a3c83d7a94330a555942b413
Wellington Matias <wellingtonmatias2005@gmail.com>
2023-12-25 10:25:58 -0300

    utilizando a estrutura for

---
+++ Dia 6/Dia 6.js
@@ -0,0 +1,21 @@
+// CRIANDO LOOP FOR
+
+let numero = Number(prompt("Insira um número inteiro e positivo: "));
+
+// CONTAGEM DO 0 AO NÚMERO INSERIDO PELO USUÁRIO
+
+for(let contador = 0; contador <= numero; contador++){
+    console.log(contador)
+}
+
+// CONTAGEM DO 0 AO 50 DE 5 EM 5
+
+for(let contador = 0; contador <= 50; contador+= 5){
+    console.log(contador)
+}
+
+// CONTAGEM DO 50 AO 0
+
+for(let contador = 50; contador >= 0; contador--){
+    console.log(contador)
+}
